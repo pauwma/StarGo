1f5afe4a1a8575036d340bab6dd7d61b3265817b
pauwma <paumual2003@elpuig.xeill.net>
2023-01-20 17:29:05 +0100

    Prueba

--- app/src/main/java/com/example/mp08_firebase/AppViewModel.java
+++ app/src/main/java/com/example/mp08_firebase/AppViewModel.java
@@ -23,5 +23,6 @@ public class AppViewModel extends AndroidViewModel {
     }
     public void setMediaSeleccionado(Uri uri, String type) {
         mediaSeleccionado.setValue(new Media(uri, type));
+
     }
 }
