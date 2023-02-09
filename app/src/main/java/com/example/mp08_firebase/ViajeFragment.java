f0300ef917bf39dea2343526546973fddca6a942
pauwma <paumual2003@elpuig.xeill.net>
2023-02-09 15:41:50 +0100

    Cambio de cerrar sesión + otros

--- app/src/main/java/com/example/mp08_firebase/ViajeFragment.java
+++ app/src/main/java/com/example/mp08_firebase/ViajeFragment.java
@@ -8,7 +8,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class BuscadorFragment extends Fragment {
+
+public class ViajeFragment extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -17,6 +18,6 @@ public class BuscadorFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_buscador, container, false);
+        return inflater.inflate(R.layout.fragment_viaje, container, false);
     }
 }
