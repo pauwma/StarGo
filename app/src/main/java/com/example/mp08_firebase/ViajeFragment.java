0960f486b821b6d68756d41e06d4346483986f84
pauwma <pauwmaa@gmail.com>
2023-02-21 11:34:15 +0100

    Multiple date

--- app/src/main/java/com/example/mp08_firebase/ViajeFragment.java
+++ app/src/main/java/com/example/mp08_firebase/ViajeFragment.java
@@ -16,19 +16,20 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Locale;
 
 
 public class ViajeFragment extends Fragment {
 
     NavController navController;
     private Button pickDateBtn;
-    private TextView selectedDate;
+    private TextView selectedDates;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    // Variable para llevar el registro de la fecha de inicio y finalización
+    private Calendar startDate = null;
+    private Calendar endDate = null;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -38,41 +39,56 @@ public class ViajeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
         pickDateBtn = view.findViewById(R.id.dateButton);
-        selectedDate = view.findViewById(R.id.dateText);
+        selectedDates = view.findViewById(R.id.dateText);
 
         pickDateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // on below line we are getting
-                // the instance of our calendar.
-                final Calendar c = Calendar.getInstance();
+                // Crear dos instancias de calendario para almacenar la fecha de inicio y finalización
+                final Calendar startDateCalendar = Calendar.getInstance();
+                final Calendar endDateCalendar = Calendar.getInstance();
 
-                // on below line we are getting
-                // our day, month and year.
-                int year = c.get(Calendar.YEAR);
-                int month = c.get(Calendar.MONTH);
-                int day = c.get(Calendar.DAY_OF_MONTH);
+                // Crear dos date picker dialog, uno para la fecha de inicio y otro para la fecha de finalización
+                DatePickerDialog.OnDateSetListener startDateListener = new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        // Actualizar la fecha de inicio y mostrar la fecha en el botón
+                        startDateCalendar.set(year, month, dayOfMonth);
+                        startDate = startDateCalendar;
+                        updateSelectedDatesText();
+                    }
+                };
 
-                // on below line we are creating a variable for date picker dialog.
-                DatePickerDialog datePickerDialog = new DatePickerDialog(
-                        // on below line we are passing context.
-                        getContext(), new DatePickerDialog.OnDateSetListener() {
-                            @Override
-                            public void onDateSet(DatePicker view, int year,
-                                                  int monthOfYear, int dayOfMonth) {
-                                // on below line we are setting date to our text view.
-                                selectedDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+                DatePickerDialog.OnDateSetListener endDateListener = new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        // Actualizar la fecha de finalización y mostrar la fecha en el botón
+                        endDateCalendar.set(year, month, dayOfMonth);
+                        endDate = endDateCalendar;
+                        updateSelectedDatesText();
+                    }
+                };
 
-                            }
-                        },
-                        // on below line we are passing year,
-                        // month and day for selected date in our date picker.
-                        year, month, day);
-                // at last we are calling show to
-                // display our date picker dialog.
-                datePickerDialog.show();
+                // Mostrar los dos date picker dialogs
+                DatePickerDialog startDatePickerDialog = new DatePickerDialog(getContext(), startDateListener,
+                        startDateCalendar.get(Calendar.YEAR), startDateCalendar.get(Calendar.MONTH), startDateCalendar.get(Calendar.DAY_OF_MONTH));
+
+                DatePickerDialog endDatePickerDialog = new DatePickerDialog(getContext(), endDateListener,
+                        endDateCalendar.get(Calendar.YEAR), endDateCalendar.get(Calendar.MONTH), endDateCalendar.get(Calendar.DAY_OF_MONTH));
+
+                startDatePickerDialog.show();
+                endDatePickerDialog.show();
             }
         });
+    }
+
+    // Actualizar el texto del botón con la fecha de inicio y finalización seleccionadas
+    private void updateSelectedDatesText() {
+        if (startDate != null && endDate != null) {
+            SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
+            selectedDates.setText(sdf.format(startDate.getTime()) + " - " + sdf.format(endDate.getTime()));
+        }
     }
 }
